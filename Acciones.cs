03c793031fa28e140ca7edba1546f8a2032341eb
Lab2-16 <Lab2-16@LAB2-16>
2025-04-29 18:54:18 -0600

    Agregar archivos de proyecto.

---
+++ Acciones.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Acciones : IAcciones
+    {
+        Alumno l = new Alumno();
+        public List<Alumno> listaalumnos = new List<Alumno>()
+        {new Alumno("Angela",20,113149)
+        };
+
+        public void Actualizar(int matricula, string nombre, int edad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Agregar(int matricula, string nombre, int edad)
+        {
+            listaalumnos.Add(new Alumno(l.Nombre = nombre, l.Edad = edad, l.Matricula = matricula));
+        }
+
+     
+        public List<Alumno> Consultar()
+        { 
+            return listaalumnos; 
+        }
+
+        public void Eliminar(int matricula)
+        {
+            var objetoeliminar = listaalumnos.FirstOrDefault(x => x.Matricula == matricula);
+            if (objetoeliminar != null)
+                listaalumnos.Remove(objetoeliminar);
+
+            else
+                Console.WriteLine("No se pudo eliminar, no hay nada");
+        }
+
+        public void ExportarExcel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        public enum Menu
+        {
+            Consultar=1, Agregar, Actualizar, Eliminar
+        }
+        static void Main(string[] args)
+        {
+            Acciones acciones = new Acciones();
+            while(true)
+            {
+                switch (Men())
+                { 
+                    case Menu.Consultar:
+                        var lista = acciones.Consultar();
+                        foreach (var l in lista)
+                        {
+                            Console.WriteLine($"Nombre:{l.Nombre}");
+                            Console.WriteLine($"Edad:{l.Edad}");
+                            Console.WriteLine($"Matricula:{l.Matricula}");
+                        }
+                        break;
+                        case Menu.Agregar:
+                        Console.WriteLine("Nombre:");
+                        string nombre = Console.ReadLine();
+                        Console.WriteLine("Edad");
+                        int edad = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Matricula");
+                        int matricula = Convert.ToInt32(Console.ReadLine());
+                        acciones.Agregar(matricula, nombre, edad);
+                        break;
+                        case Menu.Actualizar:
+                        break;
+                        case Menu.Eliminar:
+                        Console.WriteLine("Dame matricula a eliminar");
+                        int matriculaeliminar = Convert.ToInt32(Console.ReadLine());
+                        acciones.Eliminar(matriculaeliminar);
+                        break;
+                        default:
+                        break;
+                }
+
+
+            }
+        }
+        static Menu Men()
+        {
+            Console.WriteLine("1)Consultar");
+            Console.WriteLine("2)Agregar");
+            Console.WriteLine("3)Actualizar");
+            Console.WriteLine("4)Eliminar");
+            Menu opc = (Menu)Convert.ToInt32(Console.ReadLine());
+
+            return opc;
+        }
+    }
+}
